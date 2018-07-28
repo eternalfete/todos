3369e94ba4fec37752c7df91f651b66eadaf3c40
eternalfete <eternalfete@gmail.com>
2018-07-28 12:47:36 +0900

    change main.js

--- todoapp/static/todoapp/js/main.js
+++ todoapp/static/todoapp/js/main.js
@@ -1,6 +1,69 @@
 var app = new Vue({
   el: '#app',
   data: {
-    todo: 'First task'
-  }
+    todos: []
+  },
+  methods: {
+    getData: function() {
+      console.log('bb');
+      axios.get('http://localhost:8000/api/todos/')
+        .then(function(response) {
+          console.log(response);
+          this.todos = response.data;
+          console.log(this.todos);
+        }).catch(function() {
+          console.log('aa');  
+        });
+      }
+    },
+    created() {
+      console.log('created');
+      // this.getData();
+    },
+    mounted() {
+      console.log('mounted');
+      axios.get('http://localhost:8000/api/todos/')
+        .then(function(response) {
+          console.log(response);
+          this.todos = response.data;
+          console.log(this.todos);
+        }).catch(function() {
+          console.log('aa');  
+        });
+      // this.getData();
+      console.log('mounted todos:'+this.todos);
+    },
+    updated() {
+      console.log('updated');
+      console.log('updated todos:', this.todos);
+    }
 })
+
+// var app = new Vue({
+//   el: '#app',
+//   data () {
+//     return {
+//       todos: null,
+//       loading: true,
+//       errored: false
+//     }
+//   },
+//   filters: {
+//     currencydecimal (value) {
+//       return value.toFixed(2)
+//     }
+//   },
+//   mounted () {
+//     axios
+//       .get('http://localhost:8000/api/todos/')
+//       .then(response => {
+//         this.todos = response.data
+//         console.log(this.todos);
+//       })
+//       .catch(error => {
+//         console.log(error)
+//         this.errored = true
+//       })
+//       .finally(() => this.loading = false)
+//   }
+// })
